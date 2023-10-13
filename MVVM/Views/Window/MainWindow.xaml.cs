4e2045b7c83fc07b447971f450ae809d757b5524
Alexander Kolyasa <mrkyt875@gmail.com>
2023-10-13 05:40:34 +0300

    first push file to remote branch

---
+++ MVVM/Views/Window/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlockifyLauncher
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Resources.Add("WindowTitle", this.Title);
+        }
+
+        private void LoadingMainWindow(object sender, RoutedEventArgs e)
+        {
+            //this.MinecraftVerisonComboBox.Items.Add("Version 1.17.1");
+            //MinecraftVerisonComboBox.SelectedIndex = 0;
+        }
+
+        private void MainWindowsClose(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+        private void MainWindowsMinimals(object sender, RoutedEventArgs e) => Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        private void HeaderMouse(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) DragMove();
+        }
+
+    }
+}
