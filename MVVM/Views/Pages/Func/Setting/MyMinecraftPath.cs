a77c468a38c691fffc36733e3d23a12de2c0cc1c
unknown <mrkyt875@gmail.com>
2023-12-25 23:59:06 +0200

    update all file

---
+++ MVVM/Views/Pages/Func/Setting/MyMinecraftPath.cs
@@ -0,0 +1,29 @@
+﻿using CmlLib.Core;
+
+namespace BlockifyLauncher.MVVM.Views.Pages.Func.Setting
+{
+    class MyMinecraftPath : MinecraftPath
+    {
+        public MyMinecraftPath(string p)
+        {
+            BasePath = NormalizePath(p);
+
+            Library = NormalizePath(BasePath + "/libs");
+            Versions = NormalizePath(BasePath + "/vers");
+            Resource = NormalizePath(BasePath + "/resources");
+
+            Runtime = NormalizePath(BasePath + "/java");
+            Assets = NormalizePath(BasePath + "/assets");
+            CreateDirs();
+        }
+
+        public override string GetVersionJarPath(string id)
+            => NormalizePath($"{Versions}/{id}/client.jar");
+
+        public override string GetVersionJsonPath(string id)
+            => NormalizePath($"{Versions}/{id}/client.json");
+
+        public override string GetAssetObjectPath(string assetId)
+            => NormalizePath($"{Assets}/files");
+    }
+}
