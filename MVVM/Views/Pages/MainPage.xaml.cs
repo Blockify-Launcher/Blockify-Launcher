788e52ea1aac246a7209061a6bd6ece0c8246e50
Alexander Kolyasa <84909252+PalmaLuv@users.noreply.github.com>
2024-07-30 19:56:22 +0300

    Update resize element [ width ]
    Correction of changing the width of the form (main page)

--- MVVM/Views/Pages/MainPage.xaml.cs
+++ MVVM/Views/Pages/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using BlockifyLauncher.MVVM.Views.Components;
 using BlockifyLauncher.MVVM.Views.Pages.Func.Main;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -18,8 +19,10 @@ namespace BlockifyLauncher.MVVM.Views.Pages
         public MainPage()
         {
             InitializeComponent();
+            
             SetBlurContainer();
         }
+
 
         private void SetBlurContainer()
         {
@@ -27,28 +30,19 @@ namespace BlockifyLauncher.MVVM.Views.Pages
             Blur_WindowBlur.BlurContainer = mainWindow.MainBorder;
             Blur_WindowBlur_Two.BlurContainer = mainWindow.MainBorder;
         }
+        
 
-        private void ResizeStackPanelNew(object sender, SizeChangedEventArgs e)
-        {
-            if (e.WidthChanged)
-            {
-                UpdateStackPanelNews();
-            }
-        }
-
-        private void UpdateStackPanelNews()
+        /*private async void UpdateStackPanelNews()
         {
             var news = new News();
             _news = news.getAllNews();
 
             int maxItems = (int)this.ActualWidth / (ItemWidth + ItemMargin);
             StackPanelNews.Children.Clear();
-
+            
             for (int i = 0; i < maxItems && i < _news.Length; i++)
-            {
                 StackPanelNews.Children.Add(CreateNewsItem(_news[i]));
-            }
-        }
+        }*/
 
         private NewsComponent CreateNewsItem(NewsStr __news)
         {
@@ -59,6 +53,53 @@ namespace BlockifyLauncher.MVVM.Views.Pages
 
         private void LoadingMainPage(object sender, RoutedEventArgs e)
         {
+            InitializeNews();
         }
+
+        // Initialize news list.
+        private void InitializeNews()
+        {
+            var news = new News();
+            _news = news.getAllNews();
+
+            StackPanelNews.Children.Clear();
+
+            for (int i = 0; i < _news.Length; i++)
+                StackPanelNews.Children.Add(CreateNewsItem(_news[i]));
+
+            UpdateStackPanelNews(); // Update visual element.
+        }
+
+        // Count visible panel element.
+        private static int GetVisibleElementCount(StackPanel panel)
+        {
+            int visibleElementCount = 0;
+
+            foreach (UIElement element in panel.Children)
+                if (element.Visibility == Visibility.Visible)
+                    visibleElementCount++;
+
+            return visibleElementCount;
+        }
+
+        // Resize static panel.
+        private async void ResizeStackPanelNew(object sender, SizeChangedEventArgs e)
+        {
+            if ((int)this.ActualWidth / (ItemWidth + ItemMargin) != GetVisibleElementCount(StackPanelNews))
+                if (e.WidthChanged)
+                {
+                    UpdateStackPanelNews();
+                }
+        }
+
+        // Update resize.
+        private async void UpdateStackPanelNews()
+        {
+            int maxElement = (int)this.ActualWidth / (ItemWidth + ItemMargin);
+            for (int i = 0; i < StackPanelNews.Children.Count; i++)
+                StackPanelNews.Children[i].Visibility =
+                    i < maxElement ? Visibility.Visible : Visibility.Collapsed;
+        }
+
     }
 }
